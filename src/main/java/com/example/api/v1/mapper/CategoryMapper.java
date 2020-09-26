89b3866f1869681fdde5b8cddbbf5eeebf2517cc
Tomasz Białek <bialek.tomasz225@gmail.com>
2020-09-26 12:42:42 +0200

    CategoryService

--- src/main/java/com/example/api/v1/mapper/CategoryMapper.java
+++ src/main/java/com/example/api/v1/mapper/CategoryMapper.java
@@ -3,6 +3,7 @@ package com.example.api.v1.mapper;
 import com.example.api.v1.model.CategoryDTO;
 import com.example.domain.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
---
+++ src/main/java/com/example/services/CategoryService.java
@@ -0,0 +1,13 @@
+package com.example.services;
+
+import com.example.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+
+}
